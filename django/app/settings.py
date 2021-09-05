e16591c821fa3c3835bb09713745c3c008426c2d
alexisflach <alexis.flach87@gmail.com>
2021-09-05 15:39:13 +0200

    setup db and pgadmin

--- django/app/settings.py
+++ django/app/settings.py
@@ -78,9 +78,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
